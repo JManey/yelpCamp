d1fb11af7980732a10b238df1c8133e43efc36d4
JManey <jdm216@gmail.com>
2020-06-26 00:01:22 -0500

    adds in some bootstrap 3 styling

--- app.js
+++ app.js
@@ -22,6 +22,30 @@ let campgrounds = [
     name: "lake austin ",
     image: "https://live.staticflickr.com/3396/3653409750_cde4150238_b.jpg",
   },
+  {
+    name: "salmon creek",
+    image: "https://live.staticflickr.com/4156/34533119056_51fca34dff_b.jpg",
+  },
+  {
+    name: "red deer creek",
+    image: "https://live.staticflickr.com/7570/16155826112_007ffaf4ea_b.jpg",
+  },
+  {
+    name: "lake austin ",
+    image: "https://live.staticflickr.com/3396/3653409750_cde4150238_b.jpg",
+  },
+  {
+    name: "salmon creek",
+    image: "https://live.staticflickr.com/4156/34533119056_51fca34dff_b.jpg",
+  },
+  {
+    name: "red deer creek",
+    image: "https://live.staticflickr.com/7570/16155826112_007ffaf4ea_b.jpg",
+  },
+  {
+    name: "lake austin ",
+    image: "https://live.staticflickr.com/3396/3653409750_cde4150238_b.jpg",
+  },
 ];
 
 // ********************************
